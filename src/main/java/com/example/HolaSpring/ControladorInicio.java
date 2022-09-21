96cf08fd3e42b8c98bda03d3c38d8f958fcc1175
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-21 08:51:54 -0500

    Sending info through thymeleaf

--- src/main/java/com/example/HolaSpring/ControladorInicio.java
+++ src/main/java/com/example/HolaSpring/ControladorInicio.java
@@ -2,14 +2,17 @@ package com.example.HolaSpring;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
     @GetMapping("/")
-    public String inicio(){
-        log.info("Ejecutando el controlador REST");
-        return "Hola mundo con Spring Boot";
+    public String inicio(Model model) {
+        var mensaje = "Mensaje con Thymeleaf";
+        log.info("Ejecutando el controlador Spring MVC");
+        model.addAttribute("mensaje", mensaje);
+        return "index";
     }
 }
